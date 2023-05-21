3ed54d98ae9d30b8aa733a999b73d18be9a415d2
ML <kite.616@gmail.com>
2023-05-21 20:11:22 +0200

    Initial
    Initial

---
+++ Program_After.cs
@@ -0,0 +1,60 @@
+﻿namespace ESA5;
+
+public class Character
+{
+    protected Character(Weapon weapon, string name = "Character", int id = 0, int health = 1)
+    {
+        Id = id;
+        Name = name;
+        Health = health;
+        CurrentWeapon = weapon;
+    }
+
+    protected Character()
+    {
+        throw new NotImplementedException();
+    }
+
+    private int Id { get; set; }
+    private string Name { get; set; }
+    private int Health { get; set; }
+    private int TargetId { get; set; }
+    private Weapon CurrentWeapon { get; set; }
+
+    public void TakeDamage(int damage)
+    {
+        Health -= damage;
+    }
+    
+    public void SelectNewTarget(int targetId)
+    {
+        TargetId = targetId;
+    }
+}
+
+public class Weapon
+{
+    private int WeaponDamage { get; set; }
+    private int WeaponTypeId { get; set; }
+    
+    public int DealDamage()
+    {
+        return WeaponDamage * WeaponTypeId;
+    }
+}
+
+public class Player : Character
+{
+    public void SpeakToTarget()
+    {
+        //
+    }
+}
+
+public class NonPlayer : Character
+{
+    public string giveNextQuest()
+    {
+        return "QuestTitle";
+    }
+}
---
+++ Program_Before.cs
@@ -0,0 +1,80 @@
+﻿namespace ESA5Before
+{
+    class Player
+    {
+        public Player(int id = 0, string name = "Player", int health = 1, int weaponDamage = 0, int weaponTypeId = 0)
+        {
+            Id = id;
+            Name = name;
+            Health = health;
+            WeaponDamage = weaponDamage;
+            WeaponTypeId = weaponTypeId;
+        }
+
+        private int Id
+        { get; set; }
+        private string Name
+        { get; set; }
+        private int Health
+        { get; set; }
+        private int WeaponDamage
+        { get; set; }
+        private int WeaponTypeId
+        { get; set; }
+        private int TargetId
+        { get; set; }
+
+        public void TakeDamage(int damage)
+        {
+            Health -= damage;
+        }
+        public int DealDamage()
+        {
+            return WeaponDamage * WeaponTypeId;
+        }
+
+        public void SpeakToTarget()
+        {
+            //
+        }
+    }
+
+    class Npc
+    {
+        public Npc(int id = 0, string name = "NPC", int health = 1, int weaponDamage = 0, int weaponTypeId = 0)
+        {
+            Id = id;
+            Name = name;
+            Health = health;
+            WeaponDamage = weaponDamage;
+            WeaponTypeId = weaponTypeId;
+        }
+
+        private int Id
+        { get; set; }
+        private string Name
+        { get; set; }
+        private int Health
+        { get; set; }
+        private int WeaponDamage
+        { get; set; }
+        private int WeaponTypeId
+        { get; set; }
+        private int TargetId
+        { get; set; }
+
+        public void TakeDamage(int damage)
+        {
+            Health -= damage;
+        }
+        public int DealDamage()
+        {
+            return WeaponDamage * WeaponTypeId;
+        }
+
+        public string giveNextQuest()
+        {
+            return "QuestTitle";
+        }
+    }
+}
